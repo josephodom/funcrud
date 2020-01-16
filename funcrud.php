d32bd10d877c96ffd08ffdd05476443884b19728
josephodom <ejosephodom@gmail.com>
2020-01-16 17:11:54 -0500

    Created function skeletons

---
+++ funcrud.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace FunCRUD
+{
+	function create($pdo, $table, $values)
+	{
+		//
+	}
+	
+	function read($pdo, $table, $params)
+	{
+		//
+	}
+	
+	function readID($pdo, $table, $id)
+	{
+		//
+	}
+	
+	function update($pdo, $table, $where, $values)
+	{
+		//
+	}
+	
+	function updateID($pdo, $table, $id, $values)
+	{
+		//
+	}
+	
+	function delete($pdo, $table, $where, $values)
+	{
+		//
+	}
+	
+	function deleteID($pdo, $table, $id)
+	{
+		//
+	}
+}
+
+?>
